b39cb961f9119507e5117d3b13eb3486d7ae533e
muhammad yulasfi pahrizal <muhammadpahrizal790@gmail.com>
2023-07-07 14:19:33 +0800

    uses config file instead of enviroment variable

--- src/services/redis/CacheService.js
+++ src/services/redis/CacheService.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 const redis = require('redis');
+const config = require('../../utils/config');
 
 class CacheService {
   constructor() {
     this._client = redis.createClient({
       socket: {
-        host: process.env.REDIS_SERVER,
+        host: config.redis.host,
       },
     });
     this._client.on('error', (error) => {
